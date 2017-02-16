aa5a4195af9627959b9166e419943d954ca4142e
fengmk2 <fengmk2@gmail.com>
2017-02-17 00:39:19 +0800

    chore: add full profile

--- app.js
+++ app.js
@@ -16,13 +16,13 @@ module.exports = app => {
     const user = {
       provider: 'weibo',
       id: profile.id,
-      name: profile.name,
+      name: profile._json && profile._json.name,
       displayName: profile.displayName,
       photo: profile._json && profile._json.avatar_hd && profile._json.avatar_hd.replace('http://', 'https://'),
       accessToken,
       refreshToken,
       params,
-      profile: profile._json,
+      profile,
     };
 
     debug('%s %s get user: %j', req.method, req.url, user);
