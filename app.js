1574e07609767af4382b079259e31ae4ab58b147
fengmk2 <fengmk2@gmail.com>
2017-02-17 00:11:51 +0800

    init

---
+++ app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const debug = require('debug')('egg-passport-weibo');
+const assert = require('assert');
+const Strategy = require('passport-weibo').Strategy;
+
+module.exports = app => {
+  const config = app.config.passport.weibo;
+  config.passReqToCallback = true;
+  assert(config.clientID, '[egg-passport-weibo] config.passport.weibo.clientID required');
+  assert(config.clientSecret, '[egg-passport-weibo] config.passport.weibo.clientSecret required');
+
+  // must require `req` params
+  app.passport.use(new Strategy(config, function(req, accessToken, refreshToken, params, profile, done) {
+    // format user
+    const user = {
+      provider: 'weibo',
+      id: profile.id,
+      name: profile.name,
+      displayName: profile.displayName,
+      photo: profile._json && profile._json.avatar_hd && profile._json.avatar_hd.replace('http://', 'https://'),
+      accessToken,
+      refreshToken,
+      params,
+      profile: profile._json,
+    };
+
+    debug('%s %s get user: %j', req.method, req.url, user);
+
+    // let passport do verify and call verify hook
+    app.passport.doVerify(req, user, done);
+  }));
+};
---
+++ config/config.default.js
@@ -0,0 +1,9 @@
+'use strict';
+
+exports.passport = {
+  weibo: {
+    clientID: '',
+    clientSecret: '',
+    callbackURL: '/passport/weibo/callback',
+  },
+};
